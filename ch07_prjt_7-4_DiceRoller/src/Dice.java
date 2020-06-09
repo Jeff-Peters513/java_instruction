cfc9427915bc24c2cb7bf51c0243322a53bcad49
Jeff-Peters513 <petersjeff288@gmail.com>
2020-06-09 10:33:04 -0400

    corrected code made it look good and confirmed it worked as desired

--- ch07_prjt_7-4_DiceRoller/src/Dice.java
+++ ch07_prjt_7-4_DiceRoller/src/Dice.java
@@ -7,7 +7,15 @@ public class Dice {
 	public Dice() {
 		super();
 		die1 = new Die();
-		die2= new Die();
+		die2 = new Die();
+	}
+
+	public int getDie1Value() {
+		return die1.getvalue();
+	}
+
+	public int getDie2Value() {
+		return die2.getvalue();
 	}
 
 	public int getSum() {
@@ -15,24 +23,32 @@ public class Dice {
 		return sum;
 	}
 
-	public void rool() {
+	public void roll() {
 		die1.roll();
-		die1.roll();
+		die2.roll();
 	}
 
 	public void printRoll() {
-		// display 
-		System.out.println("Total: "+getSum());
-		// total and special message if nec
-		if (getSum() == 2) {
+		int total = getSum();
+		// display
+		System.out.println();
+		System.out.println("Die 1: " + die1.getvalue());
+		System.out.println("Die 2: " + die2.getvalue());
+		System.out.println("Total: " + total);
+		// total and special message if necessary
+		switch (total) {
+		case 2:
 			System.out.println("Snake Eyes!");
-		}else if (getSum() == 6) {
-				System.out.println("Box Cars!");
-		}else if (getSum() == 7) {
-					System.out.println("ojfjoiejf");
-			}
+			break;
+		case 7:
+			System.out.println("Craps!");
+			break;
+		case 12:
+			System.out.println("Box cars!");
+			break;
 		}
-		
-	
+		System.out.println();
+
 	}
 
+}
--- ch07_prjt_7-4_DiceRoller/src/DiceRollerApp.java
+++ ch07_prjt_7-4_DiceRoller/src/DiceRollerApp.java
@@ -1,25 +1,34 @@
+import java.util.Scanner;
 
 public class DiceRollerApp {
 
 	public static void main(String[] args) {
-		//welcome message
+		// welcome message
 		System.out.println("Welcome to the Dice Roller!");
+		System.out.println("");
+
+		// call Scanner
+		Scanner sc = new Scanner(System.in);
 		
 		// prompt user to roll dice: Y or y
 		String choice = Console.getString("Roll the dice? (y/n)");
-		
+
+		Dice dice = new Dice();
+
 		while (choice.equalsIgnoreCase("y")) {
-		// biz logic sum the dice
-		Dice d = new Dice();
-		d.roll();
-				
-		// display results = total and message for some results
-		d.printRoll();
-		choice = Console.getString("Roll again? (y/n)");
-	}
-		
-		//bye
+			// business logic sum the dice
+			dice.roll();
+			// display results = total and message for some results
+			dice.printRoll();
+			choice = Console.getString("Roll again? (y/n)");
+		}
+
+		// bye
+		System.out.println("");
 		System.out.println("Bye");
+		System.out.println("");
+		// close scanner resource
+		sc.close();
 	}
 
 }
