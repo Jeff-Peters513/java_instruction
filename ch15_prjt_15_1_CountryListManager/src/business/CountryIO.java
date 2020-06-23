e20dee5617bc34284410db1a90a85d6496a14103
Jeff-Peters513 <petersjeff288@gmail.com>
2020-06-23 12:36:56 -0400

    completed project 15-1

--- ch15_prjt_15_1_CountryListManager/src/business/CountryIO.java
+++ ch15_prjt_15_1_CountryListManager/src/business/CountryIO.java
@@ -1,8 +1,77 @@
 package business;
 
+import java.io.*;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CountryIO {
 
-	private String Country;
+	// this class represents I/O to and from our countries.txt file
+	// it will store the countries in an ArrayList
+	// named countries and will write to
+	// countries.txt whenever we change the list
+
+	// Reference page 481 - ProductTextFile for an example
+	private List<String> countries = null;
+	private Path countriesPath = null;
+	private File countriesFile = null;
+
+	// constructor
+	public CountryIO() {
+		countriesPath = Paths.get("countries.txt");
+		countriesFile = countriesPath.toFile();
+		getCountries();
+	}
+
+	public List<String> getCountries() {
+		// only read the countries.txt file if (array of) countries is null
+		if (countries == null) {
+			countries = new ArrayList<>();
+			if (Files.exists(countriesPath)) {
+				try (BufferedReader in = new BufferedReader(new FileReader(countriesFile))) {
+					// read countries from file into array list
+					String line = in.readLine();
+					while (line != null) {
+						countries.add(line);
+						line = in.readLine();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			} else {
+				// create the file "countries.txt"
+				try {
+					Files.createFile(countriesPath);
+					System.out.println("** countries file created!!");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return countries;
+	}
 	
+	public boolean saveCountries() {
+		boolean success = false;
+		try (PrintWriter out =  new PrintWriter(
+								new BufferedWriter(
+								new FileWriter(countriesFile)))){
+			//write all countries from our list to the file
+			for (String country: countries) {
+				out.println(country);
+			}
+			success = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return success;
+	}
 	
+	public void addCountry(String country) {
+		countries.add(country);
+		saveCountries();		
+	}
+
 }
--- ch15_prjt_15_1_CountryListManager/src/ui/CountryListManagerApp.java
+++ ch15_prjt_15_1_CountryListManager/src/ui/CountryListManagerApp.java
@@ -1,102 +1,57 @@
 package ui;
 
-import java.util.List;
-
 import business.CountryIO;
-import business.DAO;
 import util.Console;
 
 public class CountryListManagerApp {
 
-	 // declare class variables
-    private static DAO<CountryIO> countryDAO = null;
+	public static void main(String[] args) {
+		// display a welcome message
+		System.out.println("Welcome to the Country List Manager App! \n");
 
-    public static void main(String[] args) {
-        // display a welcome message
-        System.out.println("Country List Manager\n");
+		CountryIO countryIO = new CountryIO();
+		int menuNbr = 0;
 
-        // set the class variables
-        countryDAO = new CustomerTextFile();
+		while (menuNbr != 3) {
+			// start loop
+			// display menu
+			displayMenu();
 
-        // display the command menu
-        displayMenu();
+			// prompt user for input
+			menuNbr = Console.getInt("Enter menu action number:  ", 0, 4);
+			System.out.println();
 
-        // perform 1 or more actions
-        String action = "";
-        while (!action.equalsIgnoreCase("exit")) {
-            // get the input from the user
-            action = Console.getString("Enter menu number: ");
-            System.out.println();
+			// business logic
+			// display results
+			switch (menuNbr) {
+			case 1:
+				for (String country : countryIO.getCountries()) {
+					System.out.println(country);
+				}
+				System.out.println();
+				break;
+			case 2:
+				String country = Console.getString("Enter Country:  ", true);
+				countryIO.addCountry(country);
+				System.out.println("This country has been saved.");
+				System.out.println();
+				break;
+			case 3:
+				// do nothing, exit app
+				break;
 
-            if (action.equalsIgnoreCase("list")) {
-                displayAllCustomers();
-            } else if (action.equalsIgnoreCase("add")) {
-                addCustomer();
-            } else if (action.equalsIgnoreCase("del") || action.equalsIgnoreCase("delete")) {
-                deleteCustomer();
-            } else if (action.equalsIgnoreCase("help") || action.equalsIgnoreCase("menu")) {
-                displayMenu();
-            } else if (action.equalsIgnoreCase("exit")) {
-                System.out.println("Bye.\n");
-            } else {
-                System.out.println("Error! Not a valid command.\n");
-            }
-        }
-    }
+			} // end loop
 
-    public static void displayMenu() {
-        System.out.println("COMMAND MENU");
-        System.out.println("list    - List countries");
-        System.out.println("add     - Add a country");
-        //System.out.println("del     - Delete a customer");
-        //System.out.println("help    - Show this menu");
-        System.out.println("exit"); //    - Exit this application\n");
-    }
+		}
+		System.out.println("Bye");
+	}
 
-    public static void displayAllCustomers() {
-        System.out.println("CUSTOMER LIST");
+	public static void displayMenu() {
+		System.out.println("COMMAND MENU");
+		System.out.println("====================");
+		System.out.println("1 - List countries");
+		System.out.println("2 - Add a country");
+		System.out.println("3 -  exit");
+	}
 
-        List<Counrty> customers = counrtyDAO.getAll();
-        Country c;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < counrty.size(); i++) {
-            c = country.get(i);
-            sb.append(StringUtils.padWithSpaces(
-                    c.getName(), 27));
-            sb.append(c.getEmail());
-            sb.append("\n");
-        }
-        System.out.println(sb.toString());
-    }
-
-    public static void addCustomer() {
-        String firstName = Console.getString("Enter first name: ");
-        String lastName = Console.getString("Enter last name: ");
-        String email = Console.getString("Enter customer email: ");
-
-        Customer customer = new Customer();
-        customer.setFirstName(firstName);
-        customer.setLastName(lastName);
-        customer.setEmail(email);
-        customerDAO.add(customer);
-
-        System.out.println();
-        System.out.println(firstName + " " + lastName
-                + " has been added.\n");
-    }
-
-//    public static void deleteCustomer() {
-//        String email = Console.getString("Enter email to delete: ");
-//
-//        Customer c = customerDAO.get(email);
-//
-//        System.out.println();
-//        if (c != null) {
-//            customerDAO.delete(c);
-//            System.out.println(c.getName()
-//                    + " has been deleted.\n");
-//        } else {
-//            System.out.println("No customer matches that email.\n");
-//        }
-//    }
 }
