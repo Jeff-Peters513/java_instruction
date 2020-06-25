44c74292eef4e3e70dabe23eab9b4a7b1fa5e8a3
Jeff-Peters513 <petersjeff288@gmail.com>
2020-06-24 23:42:17 -0400

    lambda example completed

--- ch22_ex1_BookCatalog/src/BookManager.java
+++ ch22_ex1_BookCatalog/src/BookManager.java
@@ -3,43 +3,21 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class BookManager {
-    private List<Book> bookList;
-    
-    public BookManager() {
-        bookList = new BookCatalog().getCatalog();
-    }
-    
-    public List<Book> getBooksByTitle(String title) {
-        List<Book> books = new ArrayList<>();
-        for (Book b : bookList) {
-            if (b.getTitle().equals(title)) {
-                books.add(b);
-            }
-        }
-        return books;
-    }
-    
-    public List<Book> getBooksByCategory(String category) {
-        List<Book> books = new ArrayList<>();
-        for (Book b : bookList) {
-            if (b.getCategory().equals(category)) {
-                books.add(b);
-            }
-        }
-        return books;
-    }
-    
-    public List<Book> getBooksByFormat(String format) {
-        List<Book> books = new ArrayList<>();
-        for (Book b : bookList) {
-            if (b.getFormat().equals(format)) {
-                books.add(b);
-            }
-        }
-        return books;
-    }
-    
-    public static getBooks(Predicate<Book>)
-    
-    
+	private List<Book> bookList;
+
+	public BookManager() {
+		bookList = new BookCatalog().getCatalog();
+	}
+
+	// page 723 #4
+	public List<Book> getBooks(Predicate<Book> condition) {
+		List<Book> books = new ArrayList<>();
+		for (Book b : bookList) {
+			if (condition.test(b)) {
+				books.add(b);
+			}
+		}
+		return books;
+	}
+
 }
